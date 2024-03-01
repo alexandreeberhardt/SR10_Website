7c1343fa3987b85701d2af6e7f89ed26aac868cf
leo <leopold@lchappuis.fr>
2024-03-01 11:47:25 +0100

    refactored files

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,12 @@
+<script>
+import "../styles/LoginForm.css"
+export default {
+  name: 'App',
+}
+</script>
+<template>
+  <p>Formulaire</p>
+</template>
+
+
+
---
+++ src/pages/Login.vue
@@ -0,0 +1,17 @@
+<script>
+import "../styles/Login.css"
+import LoginForm from "../components/LoginForm.vue";
+export default {
+  name: 'App',
+  components: {
+    LoginForm
+  },
+}
+</script>
+<template>
+  <p>LoginPage</p>
+  <LoginForm/>
+</template>
+
+
+
