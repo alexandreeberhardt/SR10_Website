1e835bb85ae8d28d0da9094a5475f76c0e3ade07
leo <leopold@lchappuis.fr>
2024-03-01 11:47:25 +0100

    refactored files

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,17 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import LoginPage from './pages/Login.vue'
+import "./styles/App.css"
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LoginPage
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+  <LoginPage/>
+</template>
+
+
+
