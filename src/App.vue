9ab13b95ef514a4858f1d60395f8be76a9ad3458
leo <leopold@lchappuis.fr>
2024-03-03 19:04:41 +0100

    added router

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,14 @@
 <script>
-import LoginPage from './pages/Login.vue'
-import HomeCandidat from './pages/HomeCandidat.vue'
 import "./styles/App.css"
 export default {
   name: 'App',
   components: {
-    LoginPage,
-    HomeCandidat
   }
 }
 </script>
 
 <template>
-  <LoginPage/>
+  <router-view/>
 </template>
 
 
