72b55e87c2306fffe1a8c6eb898bfc518bf8a373
leo <leopold@lchappuis.fr>
2024-03-03 18:47:07 +0100

    added page

---
+++ src/pages/HomeCandidat.vue
@@ -0,0 +1,14 @@
+<script>
+import "../styles/Login.css"
+export default {
+  name: 'HomeCandidat',
+  components: {
+  },
+}
+</script>
+<template>
+  <div>
+    <p>Home Candidat</p>
+  </div>
+
+</template>
