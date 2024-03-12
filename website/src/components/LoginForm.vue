92ee6fbcfff6eefbe2430db62ddfd239d0c11e9c
leo <leopold@lchappuis.fr>
2024-03-12 14:09:33 +0100

    refacto files

---
+++ website/src/components/LoginForm.vue
@@ -0,0 +1,26 @@
+<script>
+import "../styles/LoginForm.css"
+export default {
+  name: 'LoginForm',
+}
+</script>
+<template>
+  <div class="container">
+    <div class="columng">
+      <h1>Connexion</h1>
+      <p>Se connecter pour acceder au site</p>
+      <form id="login">
+      <input type="text" id="email" name="email" placeholder="Email"><br><br>
+      <input type="password" id="mdp" name="mdp" placeholder="mdp" ><br><br>
+      <input type="submit" value="Connexion">
+      </form>
+      <p>-----Autre connexion-----</p>
+    </div>
+    <div class="columnd">
+    aaa
+    </div>
+  </div>
+</template>
+
+
+
