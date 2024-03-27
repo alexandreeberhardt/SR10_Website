43adcd4261eb2c075a3cb61bb67e235c1ec01f7f
leo <leopold@lchappuis.fr>
2024-03-27 17:09:38 +0100

    added basic route

--- website/routes/users.js
+++ website/routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var userModel = require('../model/user');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.get('/userslist', function (req, res, next) {
+  console.log("coucou je suis là");
+    result=userModel.readall(function(result){
+    res.render('usersList', { title: 'List des utilisateurs', users:
+    result });
+  });
 });
 
 module.exports = router;
