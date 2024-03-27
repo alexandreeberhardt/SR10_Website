fb4c81e3441124ce822af4fa7b4ca6f78549f313
leo <leopold@lchappuis.fr>
2024-03-27 18:29:48 +0100

    added post req

--- website/routes/users.js
+++ website/routes/users.js
@@ -1,18 +1,40 @@
 var express = require('express');
 var router = express.Router();
 var userModel = require('../model/user');
+const { removeAllListeners } = require('../model/db');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  result=userModel.readall(function(result){
+  res.render('usersList', { title: 'List des utilisateurs', users:
+  result });
+});
 });
 
 router.get('/userslist', function (req, res, next) {
-  console.log("coucou je suis là");
     result=userModel.readall(function(result){
-    res.render('usersList', { title: 'List des utilisateurs', users:
+    res.render('users/userslist', { title: 'List des utilisateurs', users:
     result });
   });
+});
+
+router.get('/creation', function (req, res, next) {
+    result=userModel.readall(function(result){
+    res.render('users/account_creation', { title: 'Création d\'un compte recr\'uT', users:
+    result });
+  });
+});
+
+router.post('/creation', function (req, res, next) {
+    var email = req.body.email;
+    var nom = req.body.nom;
+    var prenom = req.body.prenom;
+    var tel = req.body.tel;
+    var password = req.body.password;
+    result=userModel.create(email,nom,prenom,password, tel, function(result){
+      res.render('users/account_creation', { title: 'Création d\'un compte recr\'uT', users: result });
+    }
+    );
 });
 
 module.exports = router;
