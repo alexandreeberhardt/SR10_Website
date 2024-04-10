f7ae82e826c87ad27e8ca52998f890bcc24e5384
leo <leopold@lchappuis.fr>
2024-04-10 17:56:28 +0200

    updated

--- website/model/offer.js
+++ website/model/offer.js
@@ -10,8 +10,8 @@ var db = require('./db.js');
         });
     },
 
-    readall: function (callback) {
-        db.query("SELECT * from Utilisateur", function (err, results) {
+    readAll: function (state, callback) {
+        db.query("SELECT Fiche_poste.intitule, Fiche_poste.rythme_travail, Fiche_poste.salaire_min, Fiche_poste.salaire_max,Statuts_poste.statuts, Organisation.name, Lieu.pays FROM Offre INNER JOIN Fiche_poste ON Fiche_poste.id_fiche_poste = Offre.fiche_poste INNER JOIN State_offre ON State_offre.etat = Offre.etat INNER JOIN Statuts_poste ON Fiche_poste.statuts_poste = Statuts_poste.statuts INNER JOIN Offre_Organisation ON Offre_Organisation.offre = Offre.id_offre INNER JOIN Organisation ON Organisation.siret = Offre_Organisation.org INNER JOIN Lieu ON Organisation.adresse = Lieu.id_lieu WHERE State_offre.etat = ?",state, function (err, results) {
             if (err) throw err;
             callback(results);
         });
