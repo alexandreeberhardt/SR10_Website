f5d2f875c3c58784f9b0f85c7ec0c9ce3444b19c
leo <leopold@lchappuis.fr>
2024-04-10 18:15:06 +0200

    updated

--- website/routes/recruteur.js
+++ website/routes/recruteur.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var recruteurModel = require('../model/recruteur');
 var offreModel = require('../model/offer');
+const recruteur = require('../model/recruteur');
 
 
 router.get('/account_recruteur', function(req, res, next) {
@@ -17,11 +18,15 @@ router.get('/visualisation_offre', function(req, res, next) {
 });
 
 router.get('/home_recruteur', function(req, res, next) {
+  all = recruteurModel.readAllRequests("Active", "")
   result=offreModel.readAll("Active",function(result){
   res.render('recruteur/home_recruteur', { title: 'Accueil recruteur', result:
   result });
 });
 });
+
+
+
 
 
 
