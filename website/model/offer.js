4059724ee598bb73f3bc212e7fd9a163fc5f81e4
Alexandre <alexandre.eber01@gmail.com>
2024-05-02 18:10:02 +0200

    visualisation des offres en détail

--- website/model/offer.js
+++ website/model/offer.js
@@ -22,6 +22,19 @@ module.exports = {
     );
   },
 
+  offreDetail: function(id_offre, callback) {
+    const sql = `SELECT O.indications AS Indications, FP.intitule AS Intitule_Poste, FP.description AS Description_Poste, FP.salaire_min AS Salaire_Minimum, FP.salaire_max AS Salaire_Maximum, FP.rythme_travail AS Rythme_Travail, FP.responsable_hierarchique AS Responsable_Hierarchique, FP.statuts_poste AS Statut_Poste, L.adresse AS Adresse_Lieu, L.ville AS Ville, L.postcode AS Code_Postal, L.pays AS Pays, R.prenom AS Prenom_Recruteur, R.nom AS Nom_Recruteur, R.email AS Email_Recruteur, R.tel AS Telephone_Recruteur, Org.name AS Nom_Organisation FROM Offre O INNER JOIN Offre_Organisation OO ON O.id_offre = OO.offre INNER JOIN Organisation Org ON OO.org = Org.siret INNER JOIN Fiche_poste FP ON O.fiche_poste = FP.id_fiche_poste INNER JOIN Lieu L ON FP.lieu = L.id_lieu INNER JOIN Utilisateur R ON FP.recruteur = R.id_utilisateur WHERE FP.id_fiche_poste= ?;`;
+    db.query(sql, [id_offre], function (err, results) {
+        if (err) {
+            callback(err, null);
+        } else {
+            callback(null, results);
+        }
+    }); 
+    
+
+  },
+
   areValid: function (email, password, callback) {
     sql = "SELECT password FROM Utilisateur WHERE email = ?";
     rows = db.query(sql, email, function (err, results) {
@@ -34,3 +47,4 @@ module.exports = {
     });
   },
 };
+
--- website/routes/offre.js
+++ website/routes/offre.js
@@ -13,6 +13,20 @@ router.get("/offer", function (req, res, next) {
   });
 });
 
+router.get('/:id_offre', function (req, res) {
+  const id_offre = req.params.id_offre;
+  offreModel.offreDetail(id_offre, function(err, result) {
+      if (err) {
+          console.error('Error fetching offer details', err);
+          return res.status(500).send('Error fetching offer details');
+      }
+      if (result.length > 0) {
+          res.render('offres/detail_offre', { offre: result });
+      } else {
+          res.status(404).send('Offer not found');
+      }
+  });
+});
 
 
 module.exports = router;
