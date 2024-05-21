207d4d6d0326ee00dac3098ae3d6958cf7082300
Alexandre <alexandre.eber01@gmail.com>
2024-05-21 09:07:00 +0200

    fixed css acces in login page

--- website/app.js
+++ website/app.js
@@ -23,6 +23,8 @@ var adminRouter = require("./routes/admin");
 var app = express();
 app.use(session.init());
 
+app.use(express.static(path.join(__dirname, "public")));
+
 
 app.all("*", function (req, res, next) {
 
@@ -85,7 +87,6 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
