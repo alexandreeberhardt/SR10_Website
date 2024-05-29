798ad60939df4c5bbab950d3ab119b56b790050f
leo <leopold@lchappuis.fr>
2024-05-29 16:35:52 +0200

    updated admin pages

--- website/model/admin.js
+++ website/model/admin.js
@@ -43,6 +43,45 @@ module.exports = {
     });
   },
 
+
+  demandeOrgbyId: function (siret,callback) {
+    const query = `
+      SELECT
+        organisation_siret,
+        id_utilisateur,
+        nom,
+        prenom
+      FROM Recruteur_Organisation
+      INNER JOIN Utilisateur ON recruteur = Utilisateur.id_utilisateur
+      WHERE state = 'En attente' AND is_active = 1 AND organisation_siret = ?
+    `;
+    db.query(query,siret, function(err, results) {
+      if (err) throw err;
+      callback(err,results);
+    });
+  },
+
+  upgradeUser: function (role,id,callback) {
+    // 
+    const query = `
+      SELECT 
+        Utilisateur.id_utilisateur AS id,
+        Utilisateur_Roles.type_utilisateur AS role,
+        organisation,
+        reason,
+        nom,
+        prenom
+      FROM Utilisateur_Roles
+      INNER JOIN Utilisateur ON Utilisateur_Roles.id_utilisateur = Utilisateur.id_utilisateur
+      WHERE state_user = 'En attente' AND is_active = 1 AND Utilisateur.id_utilisateur = ?
+    `;
+
+    db.query(query,id, function(err, results) {
+      if (err) throw err;
+      callback(err,results);
+    });
+  },
+
   demandeOrg: function (callback) {
     const query = `
       SELECT
